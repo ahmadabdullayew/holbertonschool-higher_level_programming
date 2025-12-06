3584fa3f9b15914be607c3e583e4dac1cc154d44
ahmadabdullayew <ahmadabdullayevcontact@gmail.com>
2025-12-06 21:47:29 +0000

    Print a list of integers... in reverse!

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if my_list:  
+    if my_list:
         n = len(my_list)
         for i in range(0, n):
             print("{:d}".format(my_list[n - i - 1]))
