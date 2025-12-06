729c0f3377465c6167722ae78c7c455831c59cc5
ahmadabdullayew <ahmadabdullayevcontact@gmail.com>
2025-12-06 23:02:04 +0000

    Only by 2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    length = len(my_list)
-    list_result = []
-    booleans = [True, False]
-    for i in range(0, length):
-        list_result.append(booleans[my_list[i] % 2])
-    for i in range(0, length):
-        print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
+    return [n % 2 == 0 for n in my_list]
