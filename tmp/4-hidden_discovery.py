f95cfdc4b7cdc13d0349657aef2b4ac6ef024ea1
ahmadabdullayew <ahmadabdullayevcontact@gmail.com>
2025-12-06 16:34:17 +0000

    Who are you?

--- tmp/4-hidden_discovery.py
+++ tmp/4-hidden_discovery.py
@@ -1,13 +1,11 @@
-
-
-
-
 #!/usr/bin/python3
-import hidden_4
-
 if __name__ == "__main__":
-    for name in sorted(dir(hidden_4)):
-        if not name.startswith("__"):
-            print(name)
+    import hidden_4
 
+    all_names = dir(hidden_4)
+
+    for name in all_names:
+        if name[:2] != "__":
+            print(name)
+            
 
