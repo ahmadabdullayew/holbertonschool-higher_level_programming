15e787230bc0643cebfaa78bca7f1eecdd5c188a
root <ahmadabdullayevcontact@gmail.com>
2025-12-07 10:58:22 +0000

    Update dictionary

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary:
-        a_dictionary[key] = value
-    else:
-        a_dictionary.get(key, value)
+    a_dictionary[key] = value
     for key, value in sorted(a_dictionary.items()):
         print("{}: {}".format(key, value))
