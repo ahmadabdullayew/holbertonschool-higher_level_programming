c42382137e0330a4bc4c3913ca3fc3cbeb514e45
root <ahmadabdullayevcontact@gmail.com>
2025-12-07 12:15:30 +0000

    Best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,13 +2,4 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-        
-    best_key = None
-    best_value = -float('inf')
-    
-    for key, value in a_dictionary.items(): 
-        if value > best_value:
-            best_value = value
-            best_key = key
-            
-    return best_key
+    return max(a_dictionary, key=a_dictionary.get)
