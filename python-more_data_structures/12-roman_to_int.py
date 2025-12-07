fae4ad693a2cbbc1f108e87538da423551c04964
root <ahmadabdullayevcontact@gmail.com>
2025-12-07 12:01:08 +0000

    Roman to Integer

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,24 @@
+kk#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    result = 0
+    n = len(roman_string)
+    for i in range(n - 1):
+        current_val = roman_dict.get(roman_string[i], 0)
+        next_val = roman_dict.get(roman_string[i + 1], 0)
+        if current_val < next_val:
+            result -= current_val
+        else:
+            result += current_val
+    result += roman_dict.get(roman_string[n - 1], 0)
+    return result
