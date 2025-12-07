da3388d8da2f8aacf683cd7c3c41a5cf7d546165
root <ahmadabdullayevcontact@gmail.com>
2025-12-07 10:41:22 +0000

    Only differents

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    od_set = set.difference(set_1, set_2)
+    od_set = set.union(set_1, set_2) - set.intersection(set_1, set_2)
     return od_set
