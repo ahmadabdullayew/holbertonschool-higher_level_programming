6494a650b7be48c0252d70d8729eec0c055126ab
root <ahmadabdullayevcontact@gmail.com>
2025-12-07 09:58:54 +0000

    Squared simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[row[i] * row[i] for row in matrix] for i in range(3)]
+    new_matrix = [[row * row for row in matrix[i]] for i in range(3)]
     return new_matrix
