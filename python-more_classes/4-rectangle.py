f79d8490bf6017eff8029d6a392c04f0407fad77
ahmadabdullayew <ahmadabdullayevcontact@gmail.com>
2025-12-12 10:36:15 +0000

    Eval is magic

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -6,6 +6,8 @@
 class Rectangle:
     """Represent a rectangle."""
     def __init__(self, width=0, height=0):
+        # Use the property setters to set initial values,
+        # which will perform validation
         self.width = width
         self.height = height
 
@@ -16,6 +18,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
+        """Set the width of the rectangle."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -29,6 +32,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """Set the height of the rectangle."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -46,12 +50,14 @@ class Rectangle:
         return 2 * (self.width + self.height)
 
     def __str__(self):
-        """Return a string representation of the rectangle
-        using '#' characters."""
+        """Return the string representation of the rectangle."""
         if self.width == 0 or self.height == 0:
             return ""
-        return "\n".join('#' * self.width for _ in range(self.height))
-
+        lines = []
+        for _ in range(self.height):
+            lines.append("#" * self.width)
+        return "\n".join(lines)
+    
     def __repr__(self):
         """Return a string representation of the rectangle for reproduction."""
-        return f"Rectangle({self.width}, {self.height})"
+        return "Bye rectangle..."
