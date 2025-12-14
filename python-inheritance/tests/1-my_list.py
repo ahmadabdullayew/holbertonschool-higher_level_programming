b856d73d5117c7077031950cb1577d74b209fa63
root <ahmadabdullayevcontact@gmail.com>
2025-12-14 17:37:39 +0000

    My list

---
+++ python-inheritance/tests/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a MyList class that inherits from list"""
+
+
+class MyList(list):
+    """MyList class inheriting from list"""
+
+    def print_sorted(self):
+        """Prints the list sorted in ascending order"""
+        print(sorted(self))
