d7ef1ad63c25a434e67aa90a9aec770450771112
ahmadabdullayew <ahmadabdullayevcontact@gmail.com>
2025-12-21 14:28:03 +0000

    To JSON string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Defines to_json_string function
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object as a string
+    """
+    return json.dumps(my_obj)
